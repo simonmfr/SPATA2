c4c4a8378fc9357a31ce519db70712e398d4d6eb
simonmfr <70199914+simonmfr@users.noreply.github.com>
2022-12-08 11:12:03 +0100

    added plotIasHeatmap_merge

--- R/plotH-M.R
+++ R/plotH-M.R
@@ -582,6 +582,297 @@ plotIasHeatmap <- function(object,
 
 }
 
+#' @title Plot IAS heatmap (with merge function)
+
+#'
+#' @description Plots gene expression changes against the distance
+#' an the image annotation using a heatmap. Added functionality: Plot mean of 
+#' two ids with identical dimensions. Set merge_ias=TRUE and id2="name_of_id2". 
+#'
+#' @inherit imageAnnotationScreening params details
+#' @inherit plotIasLineplot params
+#' @inherit plotTrajectoryHeatmap params
+#' @inherit ggplot_dummy return
+#' @inherit argument_dummy params
+#'
+#' @inheritSection section_dummy Distance measures
+#'
+#' @export
+
+plotIasHeatmap_merge <- function(object,
+                           id,
+                           variables,
+                           distance = NA_integer_,
+                           n_bins_circle = NA_integer_,
+                           binwidth = getCCD(object),
+                           angle_span = c(0,360),
+                           outer = TRUE,
+                           inner = TRUE,
+                           arrange_rows = "input",
+                           method_gs = "mean",
+                           smooth_span = 0.4,
+                           multiplier = 10,
+                           clrsp = "inferno",
+                           .cols = dplyr::everything(),
+                           summarize_with = "mean",
+                           .f = NULL,
+                           bcsp_exclude=NULL,
+                           include_area = FALSE,
+                           display_border = FALSE,
+                           border_linealpha = 0.75,
+                           border_linecolor = "black",
+                           border_linesize = 1,
+                           border_linetype = "dashed",
+                           verbose = NULL,
+                           merge_ias=FALSE,                                   ###### --- novel
+                           id2=NA_integer_ # must have same parameters!       ###### --- novel
+                           #...
+                           ){
+
+  hlpr_assign_arguments(object)
+
+# 1. Control --------------------------------------------------------------
+
+  # all checks
+  input_levels <- base::unique(variables)
+
+  smooth <- TRUE
+
+  # -----
+
+  # 2. Data wrangling -------------------------------------------------------
+
+  img_ann_df <-
+    getIasDf(
+      object = object,
+      id = id,
+      distance = distance,
+      binwidth = binwidth,
+      n_bins_circle = n_bins_circle,
+      n_bins_angle = 1,
+      angle_span = angle_span,
+      outer = outer,
+      inner = inner,
+      variables = variables,
+      summarize_by = "bins_circle",
+      summarize_with = summarize_with,
+      bcsp_exclude=bcsp_exclude,
+      remove_circle_bins = !include_area
+    ) %>%
+    tidyr::pivot_longer(
+      cols = dplyr::any_of(variables),
+      names_to = "variables",
+      values_to = "values"
+    )
+
+  wide_df <-
+    tidyr::pivot_wider(
+      data = img_ann_df,
+      id_cols = variables,
+      names_from = bins_circle,
+      values_from = "values"
+    )
+
+  # -----                                                                    ###### --- novel start
+
+  # option: merge multiple annotations
+
+  if(base::isTRUE(merge_ias)){
+    
+    if (!id2 %in% names(getImageAnnotations(visium))) { print(paste0("id2 not defined or not available in this spata2 object"))
+    } else {
+
+    img_ann_df2 <-
+        getIasDf(
+          object = object,
+          id = id2,
+          distance = distance,
+          binwidth = binwidth,
+          n_bins_circle = n_bins_circle,
+          n_bins_angle = 1,
+          angle_span = angle_span,
+          outer = outer,
+          inner = inner,
+          variables = variables,
+          summarize_by = "bins_circle",
+          summarize_with = summarize_with,
+          bcsp_exclude=bcsp_exclude,
+          remove_circle_bins = !include_area
+        ) %>%
+        tidyr::pivot_longer(
+          cols = dplyr::any_of(variables),
+          names_to = "variables",
+          values_to = "values"
+        )
+
+        # get mean of id1 and id2
+        m <- merge(img_ann_df, img_ann_df2, by=c("bins_circle","bins_order","variables"))
+        m <- m %>% mutate(values = rowMeans(.[, c("values.x","values.y")]))
+        m <- dplyr::select(m, bins_circle, bins_order, variables, values)
+        m <- dplyr::arrange(m, bins_order)
+        # note: order of variables changed! not sure if matters.        
+        
+        img_ann_df <- m
+        
+     }
+  }
+
+                                                                            ###### --- novel end
+ 
+  # 4. Smooth rows ----------------------------------------------------------
+
+  mtr <- base::as.matrix(dplyr::select(.data = wide_df, -variables))
+  base::rownames(mtr) <- dplyr::pull(.data = wide_df, variables)
+
+  keep <- base::apply(mtr, MARGIN = 1,
+                      FUN = function(x){
+
+                        dplyr::n_distinct(x) != 1
+
+                      })
+
+  n_discarded <- base::sum(!keep)
+
+  if(base::isTRUE(smooth) && n_discarded != 0){
+
+    discarded <- base::rownames(mtr)[!keep]
+
+    discarded_ref <- stringr::str_c(discarded, collapse = ', ')
+
+    mtr <- mtr[keep, ]
+
+    base::warning(glue::glue("Discarded {n_discarded} variables due to uniform expression. (Can not smooth uniform values.): '{discarded_ref}'"))
+
+  }
+
+  mtr_smoothed <- matrix(0, nrow = nrow(mtr), ncol = ncol(mtr) * multiplier)
+
+  base::rownames(mtr_smoothed) <- base::rownames(mtr)
+  base::colnames(mtr_smoothed) <- stringr::str_c("V", 1:base::ncol(mtr_smoothed))
+
+  if(base::isTRUE(smooth)){
+
+    confuns::give_feedback(
+      msg = glue::glue("Smoothing values with smoothing span: {smooth_span}."),
+      verbose = verbose
+    )
+
+    for(i in 1:base::nrow(mtr)){
+
+      x <- 1:base::ncol(mtr)
+
+      values <- base::as.numeric(mtr[i,])
+
+      y <- (values - base::min(values))/(base::max(values) - base::min(values))
+
+      model <- stats::loess(formula = y ~ x, span = smooth_span)
+
+      mtr_smoothed[i,] <- stats::predict(model, seq(1, base::max(x) , length.out = base::ncol(mtr)*multiplier))
+
+    }
+
+  }
+
+  # arrange rows
+  if(base::all(arrange_rows == "maxima") | base::all(arrange_rows == "minima")){
+
+    mtr_smoothed <-
+      confuns::arrange_rows(
+        df = base::as.data.frame(mtr_smoothed),
+        according.to = arrange_rows,
+        verbose = verbose
+      ) %>%
+      base::as.matrix()
+
+  } else if(arrange_rows == "input"){
+
+    mtr_smoothed <-
+      base::as.data.frame(mtr_smoothed) %>%
+      tibble::rownames_to_column(var = "vars") %>%
+      dplyr::mutate(vars = base::factor(x = vars, levels = input_levels)) %>%
+      tibble::as_tibble() %>%
+      dplyr::arrange(vars) %>%
+      base::as.data.frame() %>%
+      tibble::column_to_rownames(var = "vars") %>%
+      base::as.matrix()
+
+  }
+
+  # -----
+
+      
+  # Plot heatmap ------------------------------------------------------------
+
+  ias_levels <- base::colnames(mtr_smoothed)
+  var_levels <- base::rownames(mtr_smoothed) %>% base::rev()
+
+  df_smoothed <-
+    base::as.data.frame(mtr_smoothed) %>%
+    tibble::rownames_to_column(var = "variables") %>%
+    tibble::as_tibble() %>%
+    tidyr::pivot_longer(
+      cols = dplyr::all_of(ias_levels),
+      values_to = "values",
+      names_to = "circle_order"
+    ) %>%
+    dplyr::mutate(
+      ias_order = base::factor(x = circle_order, levels = ias_levels),
+      variables = base::factor(x = variables, levels = var_levels),
+      ias_ord_num = base::as.character(circle_order) %>% stringr::str_remove("^V") %>% base::as.numeric(),
+      ias_part = "none"
+    )
+
+  if(!base::is.null(.f)){
+
+    df_smoothed$variables <-
+      confuns::vredefine_with(
+        df_smoothed$variables,
+        .cols = .cols,
+        .f = .f
+      )
+
+  }
+
+  if(base::isTRUE(display_border)){
+
+    xintercept <- if(base::isTRUE(include_area)){ multiplier + 0.5 } else { 0.5 }
+
+    border_add_on <-
+      ggplot2::geom_vline(
+        xintercept = xintercept,
+        alpha = border_linealpha,
+        color = border_linecolor,
+        size = border_linesize,
+        linetype = border_linetype
+      )
+
+  } else {
+
+    border_add_on <- NULL
+
+  }
+
+  out <-
+    ggplot2::ggplot(data = df_smoothed) +
+    ggplot2::geom_tile(mapping = ggplot2::aes(x = ias_ord_num, y = variables, fill = values)) +
+    border_add_on +
+    ggplot2::theme_classic() +
+    ggplot2::labs(x = NULL, y = NULL, fill = "Expr.") +
+    ggplot2::theme(
+      axis.ticks = ggplot2::element_blank(),
+      axis.line.x = ggplot2::element_blank(),
+      axis.line.y = ggplot2::element_blank(),
+      axis.text.x = ggplot2::element_blank(),
+      strip.background = ggplot2::element_blank(),
+      strip.text = ggplot2::element_blank()
+    ) +
+    scale_color_add_on(aes = "fill", clrsp = clrsp)
+
+  return(out)
+
+}
+
+
 
 #' @title Plot IAS lineplot
 #'
